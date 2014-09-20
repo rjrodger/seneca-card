d33d25c8c1a14081f08e8f256a464c1ea653efc8
iantocristian <iantocristian@yahoo.com>
2014-09-20 15:01:10 +0300

    test cleanup, more refactoring

--- test/card.test.js
+++ test/card.test.js
@@ -43,6 +43,7 @@ describe('card', function() {
   it('happier', function(fin) {
 
     async.waterfall([
+      function(cb) { cb(null, {}) },
       maketop,
       memdump,
       makenote,
@@ -57,7 +58,7 @@ describe('card', function() {
     })
 
 
-    function maketop(cb) {
+    function maketop(ctx, cb) {
       si.act(
         'role:card,make:top', {
           title:'Course 0'
@@ -68,47 +69,53 @@ describe('card', function() {
           assert.equal(top.id, top.parent)
           assert.equal(0, top.children.length)
 
-          cb(null, top);
+          ctx.top = top
+
+          cb(null, ctx)
         }
       )
     }
 
-    function makenote(top, cb) {
-      var note = si.make$('card/note', {parent: top, text: 'note0'})
+    function makenote(ctx, cb) {
+      var note = si.make$('card/note', {parent: ctx.top, text: 'note0'})
       note.save$(function (err, note0) {
         if(err) return cb(err);
 
-        assert.equal(top.id,note0.parent)
+        assert.equal(ctx.top.id,note0.parent)
         assert.equal(0,note0.children.length)
 
-        cb(null, note0, top)
+        ctx.note0 = note0
+
+        cb(null, ctx)
       })
     }
 
-    function makeimg(note0, top, cb) {
-      var img = si.make$('card/img', {parent:note0,title:'Image',caption:'img 0',data:'abcdef'})
+    function makeimg(ctx, cb) {
+      var img = si.make$('card/img', {parent:ctx.note0,title:'Image',caption:'img 0',data:'abcdef'})
       img.save$(function (err, img0) {
         if(err) return cb(err);
 
-        assert.equal(note0.id,img0.parent)
+        assert.equal(ctx.note0.id,img0.parent)
         assert.equal(0,img0.children.length)
 
-        cb(null, img0, note0, top)
+        ctx.img0 = img0
+
+        cb(null, ctx)
       })
     }
 
-    function verifychildren(img0, note0, top, cb) {
+    function verifychildren(ctx, cb) {
       async.series([
-        _.partial(isfirstchild, top, note0),
-        _.partial(isfirstchild, note0, img0),
-        _.partial(hasnochildren, img0)
+        _.partial(isfirstchild, ctx.top, ctx.note0),
+        _.partial(isfirstchild, ctx.note0, ctx.img0),
+        _.partial(hasnochildren, ctx.img0)
       ], makecb(Array.prototype.slice.call(arguments)))
     }
 
-    function removenote(img0, note0, top, cb) {
+    function removenote(ctx, cb) {
       async.series([
-        _.bind(note0.remove$, note0, {id:note0.id}),
-        _.partial(hasnochildren, top)
+        _.bind(ctx.note0.remove$, ctx.note0, {id:ctx.note0.id}),
+        _.partial(hasnochildren, ctx.top)
       ], makecb(Array.prototype.slice.call(arguments)))
     }
 
@@ -130,64 +137,6 @@ describe('card', function() {
       })
     }
 
-  })
-  
-  it('happy', function(fin) {
-    si.act(
-      'role:card,make:top',{
-        title:'Course 0'
-      },
-      function(err,top){
-        if(err) return fin(err);
-
-        assert.equal(top.id,top.parent)
-        assert.equal(0,top.children.length)
-
-      ;si.make$(
-        'card/note',
-        {parent:top,text:'note0'}
-
-      ).save$(function(err,note0){
-        if(err) return fin(err);
-
-        assert.equal(top.id,note0.parent)
-        assert.equal(0,note0.children.length)
-
-      ;si.make$(
-          'card/img',
-          {parent:note0,title:'Image',caption:'img 0',data:'abcdef'}
-        
-      ).save$(function(err,img0){
-        if(err) return fin(err);
-
-        assert.equal(note0.id,img0.parent)
-        assert.equal(0,img0.children.length)
-        
-      ;si.act('role:card,cmd:children',{card:top},function(err,out){
-        if(err) return fin(err);
-        assert.equal(out.children[0].id,note0.id)
-
-      ;si.act('role:card,cmd:children',{card:note0},function(err,out){
-        if(err) return fin(err);
-        assert.equal(out.children[0].id,img0.id)
-
-      ;si.act('role:card,cmd:children',{card:img0},function(err,out){
-        if(err) return fin(err);
-        assert.equal(out.children.length,0)
-
-      ;si.act('role:mem-store,cmd:dump',function(err,out){
-        if(err) return fin(err);
-        console.log(util.inspect(out,{depth:null}))
-
-      ;note0.remove$({id:note0.id},function(err){
-        if(err) return fin(err);
-
-      ;si.act('role:mem-store,cmd:dump',function(err,out){
-        if(err) return fin(err);
-        console.log(util.inspect(out,{depth:null}))
-        fin()
-
-      }) }) }) }) }) }) }) }) })
   })
 
 })
